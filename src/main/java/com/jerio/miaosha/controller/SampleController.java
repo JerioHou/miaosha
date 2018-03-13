0b82dfbe67d950c4e18fc407e4ffad0e37f4abe3
jerio_hou <houjie20@gmail.com>
2018-03-13 21:28:58 +0800

    添加redis

--- src/main/java/com/jerio/miaosha/controller/SampleController.java
+++ src/main/java/com/jerio/miaosha/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.jerio.miaosha.controller;
 
 import com.jerio.miaosha.domain.User;
+import com.jerio.miaosha.redis.RedisService;
+import com.jerio.miaosha.redis.UserKey;
 import com.jerio.miaosha.result.Result;
 import com.jerio.miaosha.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
     @Autowired
     UserService userService;
+    @Autowired
+    RedisService redisService;
 
     @RequestMapping("/thymeleaf")
     public String  thymeleaf(Model model) {
@@ -30,4 +34,22 @@ public class SampleController {
         User user = userService.getUserById(id);
         return Result.success(user);
     }
+
+    @RequestMapping("/redis/get")
+    @ResponseBody
+    public Result<User> redisGet() {
+        User  user  = redisService.get(UserKey.getById, ""+1, User.class);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<Boolean> redisSet() {
+        User user  = new User();
+        user.setId(1);
+        user.setName("1111");
+        redisService.set(UserKey.getById, ""+1, user);//UserKey:id1
+        return Result.success(true);
+    }
+
 }
