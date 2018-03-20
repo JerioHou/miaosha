0b3264c9e566b5378aa4c31f3da3dbe8cde034de
jerio_hou <houjie20@gmail.com>
2018-03-20 23:42:57 +0800

    秒杀业务逻辑实现

---
+++ src/main/java/com/jerio/miaosha/service/GoodsService.java
@@ -0,0 +1,37 @@
+package com.jerio.miaosha.service;
+
+import com.jerio.miaosha.dao.GoodsDao;
+import com.jerio.miaosha.domain.MiaoshaGoods;
+import com.jerio.miaosha.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Jerio on 2018/3/20.
+ */
+@Service
+public class GoodsService {
+
+    @Autowired
+    GoodsDao goodsDao;
+
+    public List<GoodsVo> listGoodsVo(){
+        return goodsDao.listGoodsVo();
+    }
+
+    public GoodsVo getGoodsVoByGoodsId(long goodsId) {
+        return goodsDao.getGoodsVoByGoodsId(goodsId);
+    }
+
+    public void reduceStock(GoodsVo goods) {
+        MiaoshaGoods g = new MiaoshaGoods();
+        g.setGoodsId(goods.getId());
+        goodsDao.reduceStock(g);
+    }
+
+
+
+}
+
